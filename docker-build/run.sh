f54928de094c367c519f0f59d2873351c356827a
Fed333 <kovalchuk.roman03@gmail.com>
2022-05-08 22:33:14 +0300

    rectified script and force container removing

--- docker-build/run.sh
+++ docker-build/run.sh
@@ -4,5 +4,5 @@ port_ex=3000
 port_in=8088
 container_name=os-spring-docker-demo-in
 image_name=os-spring-docker-demo-in
-docker rm $container_name
+docker rm -f $container_name
 docker run --name $container_name -p $port_ex:$port_in $image_name
