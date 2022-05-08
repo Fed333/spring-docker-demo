9c5fb7d89a737dea238cc4c070cea8376d8101b3
Fed333 <kovalchuk.roman03@gmail.com>
2022-05-08 21:38:58 +0300

    wrote bash scripts for building and running docker container

---
+++ docker-build/build.sh
@@ -0,0 +1,2 @@
+cd ..
+docker build -t os-spring-docker-demo-in -f docker/Dockerfile .
---
+++ docker-build/run.sh
@@ -0,0 +1,8 @@
+/bin/sh build.sh
+
+port_ex=3000
+port_in=8088
+container_name=os-spring-docker-demo-in
+image_name=os-spring-docker-demo-in
+docker rm $container_name
+docker run --name $container_name -p $port_ex:$port_in $image_name
